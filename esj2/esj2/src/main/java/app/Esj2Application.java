1452b5aff50b2032c2e8645c58e55cbc1f8f997b
Estiven Foronda <estivenlopez359@gmail.com>
2025-08-13 00:18:17 -0500

    initial commit

---
+++ esj2/esj2/src/main/java/app/Esj2Application.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esj2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esj2Application.class, args);
+	}
+
+}
